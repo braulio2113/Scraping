2a38943a6839e726bad994f0e901eb95781c3061
braulio2113 <61525435+braulio2113@users.noreply.github.com>
2022-02-18 21:11:29 -0600

    Update and rename Seminuevos(Scrapy_V).py to Seminuevos(Scrapy_V_H).py

--- Seminuevos(Scrapy_V_H).py
+++ Seminuevos(Scrapy_V_H).py
@@ -1,21 +1,14 @@
-from scrapy.item import Field
-from scrapy.item import Item
 from scrapy.spiders import CrawlSpider, Rule
-from scrapy.selector import Selector
-from scrapy.loader.processors import MapCompose
 from scrapy.linkextractors import LinkExtractor
-from scrapy.loader import ItemLoader
+from scrapy.crawler import CrawlerProcess
 
 
-class Car(Item):
-    model = Field()
-    price = Field()
     
-class Vivanuncios(CrawlSpider):
-    name = "Hoteles"
+class Seminuevos(CrawlSpider):
+    name = "cars"
     custom_settings = {
         'USER_AGENT': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/71.0.3578.80 Chrome/71.0.3578.80 Safari/537.36',
-        'CLOSESPIDER_ITEMCOUNT': 50
+        'CLOSESPIDER_ITEMCOUNT': 10
     }
     start_urls = ['https://www.seminuevos.com/usados/-/autos?type_autos_moderated=moderated']
     
@@ -35,12 +28,20 @@ class Vivanuncios(CrawlSpider):
         )
     
     def parse_item(self, response):
-        sel = Selector(response)
-        item = ItemLoader(Car(), sel)
+        model = response.xpath('//div[contains(@class,"col")]/h2[contains(@class,"m-b-none")]/text()').get()
+        price = response.xpath('//meta[@itemprop="price"]/@content').get()
+        price
+        print(model)
+        print(price)
+        yield{
+            'models':model,
+            'prices':price
+            }
         
-        item.add_xpath('model', '//div[contains(@class,"col")]/h2[contains(@class,"m-b-none")]/text()')
-        item.add_xpath('price', '//meta[@itemprop="price"]/@content' , MapCompose(lambda i: i.replace('.', ',')))
-        yield item.load_item()
-        
-        
-#scrapy runspider Seminuevos(Scrapy_V).py -o seminuevos.csv -t csv
+process = CrawlerProcess({
+    'FEED_FORMAT': 'csv',
+    'FEED_URI': 'seminuevos.csv'
+    })
+process.crawl(Seminuevos)
+process.start()
+
